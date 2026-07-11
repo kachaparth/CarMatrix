2a4c764ce5872d3d13220cc850e033bc657e6469
kachaparth <kachaparth123@gmail.com>
2026-07-11 23:05:36 +0530

    chore(backend): initialize Spring Boot backend
    - Generate Spring Boot project
    - Configure Maven and project structure
    - Add initial application configuration

---
+++ CarMatrix/src/main/java/org/example/carmatrix/CarMatrixApplication.java
@@ -0,0 +1,13 @@
+package org.example.carmatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMatrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarMatrixApplication.class, args);
+    }
+
+}
---
+++ CarMatrix/src/test/java/org/example/carmatrix/CarMatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.carmatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMatrixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
